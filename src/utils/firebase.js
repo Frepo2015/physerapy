c479aa77baaa007f87f839e00f86c7b66a94ca4f
Alfredo Córdova Ríos <70955489+Frepo2015@users.noreply.github.com>
2023-05-26 14:09:25 -0600

    Add files via upload

---
+++ src/utils/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTzcgXFsEeqdywBW9sd1wFy82upj4rmho",
+  authDomain: "physerapyv1-f6f23.firebaseapp.com",
+  projectId: "physerapyv1-f6f23",
+  storageBucket: "physerapyv1-f6f23.appspot.com",
+  messagingSenderId: "567266129899",
+  appId: "1:567266129899:web:77143cba13f3ecb7dd8bae"
+};
+// Initialize Firebase
+export const initFirebase = initializeApp(firebaseConfig);
+
+export const db = getFirestore(initFirebase)
+
